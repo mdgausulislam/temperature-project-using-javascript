1ddd046c4edd57ff1d6dede8038fb39df1bf569b
unknown <gausulislam5@gmail.com>
2023-09-26 11:03:20 +0600

    update section

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 const apiKey = `484756ac6e313080be69a77ee84106c1`;
 const loadTemperature=city=>{
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     fetch(url)
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>displayTemperature(data))
 }
+const displayTemperature=data=>{
+    const temperature=document.getElementById('temperature');
+    // console.log(data.main.temp);
+    temperature.innerText=data.main.temp;
+    
+   
+}
+loadTemperature('dhaka');
